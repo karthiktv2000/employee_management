34d1e38f9e134d8d7d724addc6b9c264cceaed14
Karthik Shetty <karthiktv2001@gmail.com>
2022-10-11 12:03:04 +0530

    Commit3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { employee, employeeSchema } from './employee.schema';
 import { JwtModule } from '@nestjs/jwt';
+import { AdminModule } from './admin/admin.module';
 
+@Global()
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/employee2'),
@@ -12,8 +14,10 @@ import { JwtModule } from '@nestjs/jwt';
       { name: employee.name, schema: employeeSchema },
     ]),
     JwtModule.register({ secret: 'hard!to-guess_secret' }),
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { forwardRef, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { employee, employeeDocument } from './employee.schema';
@@ -52,7 +52,7 @@ export class AppService {
           });
         }
       } catch (error) {
-        res.end('please login again');
+        res.end('login again');
       }
     });
   }
