78c73de33b54cd4fcf8811b0ee4e16683fc08e00
Smruthirp <56838121+Smruthirp@users.noreply.github.com>
2022-10-11 14:38:55 +0530

    validation_patch-1

--- src/dto/employee.dto.ts
+++ src/dto/employee.dto.ts
@@ -1,9 +1,36 @@
+import { Equals, IsAlpha, IsEmail, IsNumber, IsPhoneNumber, MaxLength, MinLength } from "class-validator";
+
 export class registrationDTO {
+  @IsAlpha('en-US')
   name: string;
+
+  @IsEmail({
+    message: 'Must be in right email format'
+  })
   email: string;
+
+  @MinLength(8,{
+    message: 'Password: Minimum length must be 8'
+  })
   password: string;
+
+  @MinLength(10,{
+    message: 'Phone: Minimum must be 10'
+  })
+  @MaxLength(10,{
+    message: 'Phone: Minimum must be 10'
+  })
   phone: number;
+
   address: string;
+
+  @IsNumber()
   salary: number;
+
   role: string;
+  
+  @Equals(10,{
+    message: "Number leaves can not be altered by the employee"
+  })
+  availableLeaves: number;
 }
