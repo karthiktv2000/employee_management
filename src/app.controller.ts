b15c146b4d10fd387fb318ad8ad1e30ba1edc1ac
Smruthirp <56838121+Smruthirp@users.noreply.github.com>
2022-10-12 12:26:28 +0530

    Leave_patch-1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { registrationDTO } from './dto/employee.dto';
+import { leaveService } from './leave.service';
+import { Request, Response} from 'express';
+import { leaveDto } from './dto/leave.dto';
 
 @Controller()
 export class AppController {
@@ -29,6 +32,27 @@ export class AppController {
   public async dashboard(@Req() req: Request, @Res() res: Response) {
     this.appService.dashboard(req, res);
   }
+  
+  @Get('leaves')
+  public async leaves(@Req() req: Request, @Res() res: Response) {
+    try{
+      return this.leaveService.getAll(req,res);
+  } catch(err){
+    return (err)
+  }}
+
+  @Post('leaves/Apply')
+  public async leavesApply(
+    @Body() newLeave: leaveDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    try{
+      return this.leaveService.applyLeave(newLeave,req,res);
+  } catch(err){
+    return (err)
+  }
+      
   @Post('logout')
   public async logout(@Req() req: Request, @Res() res: Response) {
     this.appService.logout(req, res);
